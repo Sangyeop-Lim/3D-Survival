067aa4cccfefb53f364fbe3e0e649393d26d6cba
Sangyeop-Lim <dladla5675@gmail.com>
2025-05-19 15:23:19 +0900

    feat : 아이템과 상호작용 구현
    아이템과 상호작용 구현

---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        //if (curInteractable != null)
+        //{
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetInteractPrompt();
+        //}
+        //else
+        //{
+        //    promptText.gameObject.SetActive(false);
+        //    promptText.text = "";
+        //}
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
